0f81f46be78b5ea7f6bf552a49a5780c5d9e2fd4
Rob Derickson <rob.derickson@erickson.com>
2019-03-29 14:30:58 -0400

    Added server_port variable

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    default = 8080
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -21,8 +27,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
