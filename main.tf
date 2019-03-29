d64c5c86749b81d6f07e099eae2923aba2634412
Rob Derickson <rob.derickson@erickson.com>
2019-03-29 14:32:55 -0400

    Added public_ip output variable

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ variable "server_port" {
     default = 8080
 }
 
+output "public_ip" {
+    value = "${aws_instance.example.public_ip}"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
