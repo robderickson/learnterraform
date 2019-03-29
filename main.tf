d9d88d2d15b73f0eb6be1bd88296fbc6eabeb8fe
Rob Derickson <rob.derickson@erickson.com>
2019-03-29 14:40:45 -0400

    Create auto-scaling group instead of single instance.

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ provider "aws" {
 }
 
 variable "server_port" {
-    description = "The port the server will use for HTTP requests"
-    default = 8080
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
 }
 
 output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
+  value = "${aws_instance.example.public_ip}"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_launch_configuration" "example" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
@@ -21,6 +21,10 @@ resource "aws_instance" "example" {
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags {
     Name = "terraform-example"
@@ -35,5 +39,21 @@ resource "aws_security_group" "instance" {
     to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_autoscaling_group.example.id}"
+  min_size             = 2
+  max_size             = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
